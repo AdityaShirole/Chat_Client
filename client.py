5e4a62e9787fe94b71331c1f7820bee9d5b4d174
asdoc <asdocode@gmail.com>
2014-06-20 21:30:43 +0530

    Fixed 1 Message issue

--- client.py
+++ client.py
@@ -8,16 +8,17 @@ TCP_PORT = 5005
 BUFFER_SIZE = 4096
 MESSAGE = "Hey! What's up?"
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+while True:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
 
-msg = raw_input('< You > : ')
+	msg = raw_input('< You > : ')
 
-try:
-    s.sendall(msg)
-    data = s.recv(BUFFER_SIZE)
-    print '< Friend > : ' , str(data)
-    
-except socket.error, e:
-    print 'Send failed' + str(e)
-    sys.exit()
+	try:
+		s.sendall(msg)
+		data = s.recv(BUFFER_SIZE)
+		print '< Friend > : ' , str(data)
+		
+	except socket.error, e:
+		print 'Send failed' + str(e)
+		sys.exit()
